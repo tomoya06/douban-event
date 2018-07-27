b50d11165351d55230e0866f4e89b35de16c3341
tomoya06 <peng4766@gmail.com>
2018-07-27 23:42:41 +0800

    [DBEvent][Peng]添加活动筛选项。[TODO]尝试豆瓣抓包，根据市区获取活动/获取城市列表的API

--- services/CityPickerServices.js
+++ services/CityPickerServices.js
@@ -1,5 +1,5 @@
 import ConvertPinyin from "./../utils/pinyin";
-
+import BASE_URL from "./../utils/const";
 // const Provinces =
 // 	`河北省 山西省  辽宁省 吉林省 黑龙江省 江苏省 浙江省 安徽省 福建省 江西省` +
 // 	`山东省 河南省 湖北省 湖南省 广东省 海南省 四川省 贵州省 云南省 陕西省 甘肃省 青海省 ` +
@@ -38,7 +38,7 @@ export const MapLocations = [...MapProvinves, ...MapDirectCities];
 
 export function fetchProvinceCities(pro) {
 	const proPY = ConvertPinyin(pro);
-	const CITIES_URL = `https://api.douban.com/v2/event/list?loc=${proPY}`;
+	const CITIES_URL = `${BASE_URL}/event/list?loc=${proPY}`;
 	return new Promise((resolve, reject) => {
 		fetch(CITIES_URL)
 			.then((response) => response.json())
--- services/EventServices.js
+++ services/EventServices.js
@@ -1,14 +1,15 @@
-import ConvertPinyin from "./../utils/";
+import ConvertPinyin from "./../utils/pinyin";
 
-const EVENT_DETAILS_BASEAPI = 'https://api.douban.com/v2';
-const EVENT_DAY_TYPE = {
+import BASE_URL from "./../utils/const";
+
+export const EVENT_DAY_TYPES = {
 	FUTURE: {displayName: '未来', typeName: 'future'},
 	WEEK: {displayName: '本周', typeName: 'week'},
 	WEEKEND: {displayName: '周末', typeName: 'weekend'},
 	TODAY: {displayName: '今天', typeName: 'today'},
 	TOMORROW: {displayName: '明天', typeName: 'tomorrow'}
 }
-const EVENT_TYPE = {
+export const EVENT_TYPES = {
 	ALL: {displayName: '所有', typeName: 'all'},
 	MUSIC: {displayName: '音乐', typeName: 'music'},
 	FILM: {displayName: '电影', typeName: 'film'},
@@ -24,7 +25,7 @@ const EVENT_TYPE = {
 
 export function fetchEventDetails(id) {
 	return new Promise((resolve, reject) => {
-		fetch(`${EVENT_DETAILS_BASEAPI}/event/${id}`)
+		fetch(`${BASE_URL}/event/${id}`)
 			.then((response) => response.json())
 			.then((json) => {
 				return resolve(json);
@@ -35,9 +36,9 @@ export function fetchEventDetails(id) {
 	})
 }
 
-export function fetchCityEvent(city) {
+export function fetchCityEvent(city, day_type='', event_type='') {
 	const cityPY = ConvertPinyin(city);
-	const EVENTS_URL = `${EVENT_DETAILS_BASEAPI}/event/list?loc=${cityPY}`;
+	const EVENTS_URL = `${BASE_URL}/event/list?loc=${cityPY}&day_type=${day_type}&type=${event_type}`;
 	return new Promise((resolve, reject) => {
 		fetch(EVENTS_URL)
 			.then((response) => response.json())
