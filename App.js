e502240058c3b8de4b9b0155181b21f03e3bbd49
tomoya12 <peng4766@gmail.com>
2018-08-24 17:29:13 +0800

    add pages

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import EventDetailsScreen from "./views/screens/EventDetailsScreen";
 import LoginScreen from "./views/screens/LoginScreen";
 import EventListScreen from "./views/screens/EventListScreen";
 import CityPickerScreen from './views/screens/CityPickerScreen';
+import MyCollectionsScreen from "./views/screens/MyCollectionsScreen";
+import SettingScreen from "./views/screens/SettingScreen";
 
 import DemoScreen from "./views/screens/DemoScreen";
 
